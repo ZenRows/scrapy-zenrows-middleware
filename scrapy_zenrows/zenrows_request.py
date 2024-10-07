f9ddb0fa69e5a0ea5d9997eb7c61da4c0c97efa6
omisolaidowu <omisolaidowu@gmail.com>
2024-10-07 22:04:50 +0100

    First version, README update

--- scrapy_zenrows/zenrows_request.py
+++ scrapy_zenrows/zenrows_request.py
@@ -11,9 +11,9 @@ class ZenRowsRequest(Request):
         *args,
         **kwargs,
     ):
-        # store params as a request attribute
+
         self.params = params or {}
-        # initialize the request with the original Request class
+
         super(ZenRowsRequest, self).__init__(
             url,
             headers=headers,
@@ -22,7 +22,6 @@ class ZenRowsRequest(Request):
         )
 
     def build_zenrows_url(self, url, params):
-        # Build the URL for ZenRows API
         payload = {"url": url}
         if params:
             payload.update(params)
